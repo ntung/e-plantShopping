d855dfe992751182d123e0888cdd6eae9e178702
Tung Nguyen <nvntung@gmail.com>
2024-10-14 21:12:09 +0100

    Final task: deploy using GH pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/plantstore",
+  base: "/e-plantShopping",
   plugins: [react()],
   server: {
     watch: {
