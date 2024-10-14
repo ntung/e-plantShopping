d5a9d2ce732375874d61a81edde8544a8f379f54
Tung Nguyen <nvntung@gmail.com>
2024-10-14 21:41:14 +0100

    Fix 404

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/e-plantShopping",
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true
+export default defineConfig(({ command }) => {
+  const config = {
+    base: "/",
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true
+      }
     }
   }
+
+  if (command !== "serve") {
+    config.base = "/e-plantshopping/"
+  }
+
+  return config
 })
