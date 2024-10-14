07aadbba5c4c40cec2232a0f43f4767733d99636
Tung Nguyen <nvntung@gmail.com>
2024-10-14 15:51:45 +0100

    Turn the shopping cart to a Component

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# coding-project-template
+# FINAL PROJECT
+
+## Circle a number in SVG
+This [question](https://stackoverflow.com/questions/30970618/generating-number-circles-in-svg-how-to-centre-the-text) is helpful.
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -3,6 +3,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import {addItem} from "./CartSlice.jsx";
 import {useDispatch} from "react-redux";
+import ShoppingCartIcon from "./ShoppingCartIcon.jsx";
 
 function ProductList() {
     const [addedToCart, setAddedToCart] = useState({});
@@ -231,7 +232,7 @@ function ProductList() {
         padding: '15px',
         display: 'flex',
         justifyContent: 'space-between',
-        alignIems: 'center',
+        alignItems: 'center',
         fontSize: '20px',
     }
     const styleObjUl = {
@@ -259,6 +260,13 @@ function ProductList() {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const calculateTotalItems = (value) => {
+        return value;
+        //let min = 1, max = 20;
+        //return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -277,18 +285,7 @@ function ProductList() {
                 <div style={styleObjUl}>
                     <div><a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div><a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
-                        <h1 className='cart'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor"
-                                 height="68" width="68">
-                                <rect width="156" height="156" fill="none"></rect>
-                                <circle cx="80" cy="216" r="12"></circle>
-                                <circle cx="184" cy="216" r="12"></circle>
-                                <path
-                                    d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8"
-                                    fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" id="mainIconPathAttribute"></path>
-                            </svg>
-                        </h1>
+                    <ShoppingCartIcon quantity={calculateTotalItems(0)}/>
                     </a></div>
                 </div>
             </div>
@@ -317,7 +314,8 @@ function ProductList() {
                 )
                 :
                 (
-                    <CartItem onContinueShopping={handleContinueShopping}/>
+                    <CartItem onContinueShopping={handleContinueShopping}
+                              calculateTotalItems={calculateTotalItems}/>
                 )}
         </div>
     );
