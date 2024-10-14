8bc7a88c3c7c304b5a927b48a76b300bf0f954c6
Tung Nguyen <nvntung@gmail.com>
2024-10-14 22:03:27 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    base: "/",
+    base: "/e-plantShopping",
     plugins: [react()],
     server: {
       watch: {
