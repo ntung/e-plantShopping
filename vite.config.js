bcd4e8030c0c16ffc7b977f21d61680387545c90
Tung Nguyen <nvntung@gmail.com>
2024-10-14 22:13:54 +0100

    Get back the vite original config

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    base: "/e-plantShopping",
-    plugins: [react()],
-    server: {
-      watch: {
-        usePolling: true
-      }
-    }
-  }
-
-  if (command !== "serve") {
-    config.base = "/e-plantshopping/"
-  }
-
-  return config
+export default defineConfig({
+  base: "/e-plantShopping",
+  plugins: [react()],
 })
