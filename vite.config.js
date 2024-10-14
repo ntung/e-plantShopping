eee7cb18b3be117b87bed461a28a0b02c5cafabb
Tung Nguyen <nvntung@gmail.com>
2024-10-14 20:45:27 +0100

    Change the base name and enable hot reloading

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/plantstore",
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
